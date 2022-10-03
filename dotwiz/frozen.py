0a9d37b1e71fb9650526efc64186683f1dc7aa0c
Ritvik Nag <nagrim7@gmail.com>
2022-10-03 16:54:02 -0400

    minor changes

--- dotwiz/frozen.py
+++ dotwiz/frozen.py
@@ -24,14 +24,14 @@ class NotDotWiz:
     """
     A dummy class that returns ``self`` on attribute lookup.
 
-    The :meth:`__bool__` method ensures that an `if` check returns False:
+    The :meth:`__bool__` method ensures that an ``if`` check returns ``False``:
 
     >>> from dotwiz.frozen import NotDotWiz
     >>> dw = NotDotWiz()
     >>> if dw:  # this condition is never true
     ...     ...
 
-    Inspired by this clever ``NotDot`` implementation:
+    Inspired by this rather clever ``NotDot`` implementation:
         https://gist.github.com/akx/11dddd8fd3027a410ff7a7d228f2cff1
 
     """
