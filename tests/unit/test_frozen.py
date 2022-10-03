1941982f364c37d9d46d0709dc5bbfa90f24db7a
Ritvik Nag <nagrim7@gmail.com>
2022-10-03 16:06:47 -0400

    add test_frozen.py

---
+++ tests/unit/test_frozen.py
@@ -0,0 +1,23 @@
+import pytest
+
+from dotwiz.frozen import FrozenDotWiz, FrozenDotWizError
+
+
+def test_frozen_dot_wiz_is_immutable():
+    """
+    Test that :class:`FrozenDotWiz` is immutable, i.e. it cannot be
+    modified (easily).
+    """
+    dw = FrozenDotWiz(k1='value')
+
+    with pytest.raises(FrozenDotWizError):
+        dw.k2 = 'value'
+
+    with pytest.raises(FrozenDotWizError):
+        dw['k3'] = 'value'
+
+    with pytest.raises(FrozenDotWizError):
+        dw.update(k4='value')
+
+    with pytest.raises(FrozenDotWizError):
+        dw.setdefault('k5', 'value')
